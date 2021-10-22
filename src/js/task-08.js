e700941986d5b27a95bc739d4eec6787c6b78ee2
Myroslav Sokolov <mirossoks@gmail.com>
2021-10-22 18:35:52 +0300

    Update task-09.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
+const formEl = document.querySelector(".login-form")
+const emailEl = document.querySelector('[type="email"]')
+const passwordEl = document.querySelector('[type="password"]')
+const submitEl = document.querySelector('[type="submit"]')
 
+
+const handleClick = event => {
+    event.preventDefault();
+    
+        if (emailEl.value === "" || passwordEl.value === "") {
+    return alert('Заполните все поля');
+  }
+
+        const dataUser = {};
+      const formData = new FormData(event.currentTarget);
+
+      formData.forEach((value, name) => {
+        dataUser[name] = value;
+      });
+
+      console.log(dataUser);
+        formEl.reset()
+}
+
+formEl.addEventListener('submit', handleClick)
+
+// ---------------------------------------------------------
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+
+const nameColorRef = document.querySelector('.color')
+const buttonRef = document.querySelector('.change-color')
+const bodyColorRef = document.querySelector('body')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function hundleChangeColor() {
+  bodyColorRef.style.backgroundColor = getRandomHexColor()
+  nameColorRef.textContent = getRandomHexColor()
+}
+
+buttonRef.addEventListener("click", hundleChangeColor)
