5e699b85ed58628fcf5b16cfe1449ec30b2202b0
Myroslav Sokolov <mirossoks@gmail.com>
2021-10-22 12:00:05 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,9 +2,9 @@ const inputRel = document.querySelector("#validation-input")
 const length = inputRel.dataset.length / 1
 function handleBlur() {
     if (inputRel.value.length === length) {
-        return inputRel.classList.toggle('valid')
+        inputRel.classList.toggle('valid')
     }
-    else (inputRel.value.length !== length) { inputRel.classList.toggle('invalid') }
+    else {inputRel.classList.toggle('invalid')}
 }
-
+inputRel.addEventListener('focus', () => {inputRel.classList.remove('valid', 'invalid')})
 inputRel.addEventListener('blur', handleBlur)
